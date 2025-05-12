4b257873386ec212fce8dc980ed09095847455a5
GabrielHR0sa <rosahenriquegabriel@gmail.com>
2025-05-12 20:28:37 -0300

    Adding case service

---
+++ lib/src/service/case_service.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:health_check/src/dio_client.dart';
+import 'package:health_check/src/model/case_register_dto.dart';
+import 'package:health_check/src/model/case_search_request_dto.dart';
+
+class CaseService {
+  final Dio _dio = DioClient.dio;
+
+  Future<Response> registerCase(CaseRegisterDTO caseDTO) async {
+    try {
+      final response = await _dio.post('', data: caseDTO.toJson());
+      return response;
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Erro ao registrar caso');
+    }
+  }
+
+  Future<Response> searchCases(CaseSearchRequestDTO searchDTO) async {
+    try {
+      final response = await _dio.get(
+        '/search',
+        data:
+            searchDTO
+                .toJson(), // Importante: GET com body, compatível só se o backend aceitar — se não, converte para query params
+      );
+      return response;
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Erro ao buscar casos');
+    }
+  }
+}
